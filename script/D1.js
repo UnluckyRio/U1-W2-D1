fc97ce5d95d68119c211f93d1a09c796bd749af8
Valerio Di Felice <valesmile.99@gmail.com>
2025-06-03 13:24:09 +0200

    update

--- script/D1.js
+++ script/D1.js
@@ -11,43 +11,55 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* 
+Numeri = Rappresentano valori numerici
+Stringhe = Sono sequenze di caratteri, come parole, frasi o numeri racchiusi tra virgolette singole (' ') o doppie (" ")
+Booleani = Sono veri o falsi.
+Undefined = Significa che una variabile è stata dichiarata ma non ha ancora un valore assegnato.
+Null = Indica l'assenza intenzionale di un valore, come se fosse un "vuoto" o "niente".
+*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Valerio";
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let sum = 12 + 20;
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+myName = "Di Felice";
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let subtraction = 4 - x;
 
 /* ESERCIZIO 7
- Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
- Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
- EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ -Crea due variabili: "name1" e "name2". 
+ -Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ -Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ -EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john";
+let name2 = "John";
+// Verifica se name1 e name2 sono diversi
+let areDifferent = name1 === name2;
+// Verifica se name1 e name2 sono uguali in lowercase
+let areEqualLowercase = name1.toLowerCase() === name2.toLowerCase();
